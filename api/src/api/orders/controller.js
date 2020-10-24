68246b32053e4ed941c3b821c181ff8a9ec3f8d3
Cycl0matic <muhammadalaidy@protonmail.com>
2020-10-24 19:47:02 +0200

    crete order model schema

--- api/src/api/orders/controller.js
+++ api/src/api/orders/controller.js
@@ -1,7 +1,24 @@
-exports.create = async function ({ price, productId } ={}) {
-  return {
-    _id: "asjsbcjd0255548",
-    address: "asjsbcjdef",
-    amount: "0.01"
-  };
+const Order = require("./model");
+const Product = require("../products/model");
+
+// getting new address
+async function getNewAddress() {
+  return "randomaddress212151";
+}
+
+async function getAmountForPrice(price) {
+    return 1e8
+}
+
+exports.create = async function ({ price, productId } = {}) {
+  const product = await Product.findById(productId);
+  if (!product) throw new Error("Invalid productId");
+
+  const amount = getAmountForPrice(price);
+  const address = getNewAddress();
+  return await Order.create({
+    address: address,
+    price: price,
+    product: product,
+  });
 };
---
+++ api/src/api/orders/model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+  paymentAddress: {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    },
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Products"
+  },
+},{
+   timestamps:true 
+}
+);
+
+const Order = mongoose.model("Order", OrderSchema);
+
+module.exports = Order;
