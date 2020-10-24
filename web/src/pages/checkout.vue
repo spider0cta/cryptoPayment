9cbe9dbc3c5dde22e14a4f2d6d2a1760e5580977
Cycl0matic <muhammadalaidy@protonmail.com>
2020-10-24 18:03:04 +0200

    editing checkout.vue

--- web/src/pages/checkout.vue
+++ web/src/pages/checkout.vue
@@ -1,62 +1,83 @@
 <template>
-<div>
-  <div class="hero is-info">
-    <div class="hero-head">
-      <div class="container">
-        <navbar />
-      </div>
-    </div>
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">Checkout</h1>
-      </div>
-    </div>
-  </div>
-  <div class="section container" v-if="product">
-    <div class="columns">
-      <div class="column">
-        <figure class="image is-cover">
-          <img :src="product.info.imageLinks.thumbnail" />
-        </figure>
-        <h1 class="title">{{ product.info.title }}</h1>
-        <h2 class="subtitle">{{ product.info.authors[0] }}</h2>
-
-        <h2 class="title">${{ product.price.toFixed(2) }}</h2>
-      </div>
-      <div class="column">
-        <div class="tabs is-centered is-medium is-boxed">
-          <ul>
-            <li class="is-active"><a>Pay With Card</a></li>
-          </ul>
+  <div>
+    <div class="hero is-info">
+      <div class="hero-head">
+        <div class="container">
+          <navbar />
         </div>
-        <card-form />
+      </div>
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Checkout</h1>
+        </div>
       </div>
     </div>
+    <div class="section container" v-if="product">
+      <div class="columns">
+        <div class="column">
+          <figure class="image is-cover">
+            <img :src="product.info.imageLinks.thumbnail" />
+          </figure>
+          <h1 class="title">{{ product.info.title }}</h1>
+          <h2 class="subtitle">{{ product.info.authors[0] }}</h2>
+
+          <h2 class="title">${{ product.price.toFixed(2) }}</h2>
+        </div>
+        <div class="column">
+          <div class="tabs is-centered is-medium is-boxed">
+            <ul>
+              <li :class="{ 'is-active': isCard }">
+                <a @click="cardClicked">Pay With Card</a>
+              </li>
+              <li :class="{ 'is-active': isCrypto }">
+                <a @click="cryptoClicked">Pay with crypto</a>
+              </li>
+            </ul>
+          </div>
+          <card-form v-if="isCard == 'card'" />
+        </div>
+      </div>
+    </div>
+    <div class="container is-loading is-big" v-else></div>
   </div>
-  <div class="container is-loading is-big" v-else>
-  </div>
-</div>
 </template>
 
 <script>
-import Navbar from '@/components/navbar'
-import Products from '@/api/products'
-import Product from '@/components/product'
-import CardForm from '@/components/card-form'
+import Navbar from "@/components/navbar";
+import Products from "@/api/products";
+import Product from "@/components/product";
+import CardForm from "@/components/card-form";
 
 export default {
   components: { Navbar, Product, CardForm },
   data() {
     return {
-      product: null
-    }
+      product: null,
+      state: "card",
+    };
   },
   async mounted() {
-    const id = this.$route.params.id
-    this.product = await Products.getWithId(id)
-    console.log(this.products)
+    const id = this.$route.params.id;
+    this.product = await Products.getWithId(id);
+    console.log(this.products);
+  },
+  computed :{
+    isCard(){
+      return this.state === 'card'
+    },
+    isCrypto(){
+      return this.state === 'crypto'
+    }
   }
-}
+  methods:{
+    cardClicked(){
+      this.state = 'card'
+    },
+    cryptoClicked(){
+      this.state = 'crypto'
+    }
+  }
+};
 </script>
 
 <style lang="scss">
