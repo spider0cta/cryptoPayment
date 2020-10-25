502c16263d8a07da0b448b53d9dc39c93bf1537c
Cycl0matic <muhammadalaidy@protonmail.com>
2020-10-25 05:20:09 +0200

    Qrcode added with bip21 encoding functionality

--- web/src/components/crypto-pos.vue
+++ web/src/components/crypto-pos.vue
@@ -1,23 +1,44 @@
 <template>
-    <div>
+  <div>
+    <h4>Bay with Bitcoin using the details below</h4>
+    <div v-if="order" class="section">
+        <div class="tags has-addons">
+            <span class="tag is-primary is-large">Address:</span>
+            <span class="tag is-info is-large">{{order.paymentAddress}}</span>
+        </div>
+        <div class="tags has-addons">
+            <span class="tag is-primary is-large">Amount:</span>
+            <span class="tag is-info is-large">{{order.amount}}</span>
+        </div>
+        <qrcode :value="encode" :option="{size:300}"/>
     </div>
+  </div>
 </template>
 <script>
-import Oreders from '@/api/orders'
+import Oreders from "@/api/orders";
+import bip21 from "bip21"
 export default {
-    data() {
-        return {
-            order : null
-        }
+  data() {
+    return {
+      order: null
+    };
+  },
+  props: {
+    product: {
+      tybe: Object,
+      required: true
     },
-    props:{
-        product :{
-            tybe:Object,
-            required : true
-        }
-    },
-    async created(){
-        this.order = await Orders.create(this.product)  
+  },
+  async created() {
+    this.order = await Orders.create(this.product);
+  },
+  computed : {
+    encode() {
+        return bip21.encode(this.order.paymentAddress , {
+            amount : this.order.amount,
+            label : `BookStore Bitbooks ${this.order._id}`
+        })
     }
-}
+  }
+};
 </script>
--- web/src/main.js
+++ web/src/main.js
@@ -1,11 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import '@/styles/configure.scss'
+import VueQrcode from "@chenfengyuan/vue-qrcode";
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
 import NProgress from 'nprogress'
+
+Vue.component(VueQrcode.name, VueQrcode);
 
 Vue.config.productionTip = false
 
